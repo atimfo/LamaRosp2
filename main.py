93f1c5aefda2809704735a76b41d32c2ecf0f03a
Artem Frolov <manarchi@MacBook-Pro-Artem.local>
2023-10-20 14:33:05 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,97 @@
+import telebot
+from telebot import custom_filters
+from telebot import StateMemoryStorage
+from telebot.handler_backends import StatesGroup, State
+
+
+state_storage = StateMemoryStorage()
+
+bot = telebot.TeleBot("6658277141:AAFVmj01Kf5FdP7-stdqJ6AH3FWaV0LKrAs",
+                      state_storage=state_storage, parse_mode='Markdown')
+
+
+class PollState(StatesGroup):
+    name = State()
+    age = State()
+
+
+class HelpState(StatesGroup):
+    wait_text = State()
+
+
+text_poll = "опрос"
+text_button_1 = "Мужская"
+text_button_2 = "Женская"
+text_button_3 = "Детская"
+
+menu_keyboard = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+menu_keyboard.add(
+    telebot.types.KeyboardButton(
+        text_poll,
+    )
+)
+menu_keyboard.add(
+    telebot.types.KeyboardButton(
+        text_button_1,
+    )
+)
+
+menu_keyboard.add(
+    telebot.types.KeyboardButton(
+        text_button_2,
+    ),
+    telebot.types.KeyboardButton(
+        text_button_3,
+    )
+)
+
+
+@bot.message_handler(state="*", commands=['start'])
+def start_ex(message):
+    bot.send_message(
+        message.chat.id,
+        '*Добрый день!* Что будем заказывать?',
+        reply_markup=menu_keyboard)
+
+@bot.message_handler(func=lambda message: text_poll == message.text)
+def first(message):
+    bot.send_message(message.chat.id, 'Отлично! *Ваше* _имя_?')
+    bot.set_state(message.from_user.id, PollState.name, message.chat.id)
+
+
+@bot.message_handler(state=PollState.name)
+def name(message):
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data['name'] = message.text
+    bot.send_message(message.chat.id, 'Хорошо! [Ваш](https://www.example.com/) `возраст?`')
+    bot.set_state(message.from_user.id, PollState.age, message.chat.id)
+
+
+@bot.message_handler(state=PollState.age)
+def age(message):
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data['age'] = message.text
+    bot.send_message(message.chat.id, 'Благодарю за регистрацию!', reply_markup=menu_keyboard)
+    bot.delete_state(message.from_user.id, message.chat.id)
+
+
+@bot.message_handler(func=lambda message: text_button_1 == message.text)
+def help_command(message):
+    bot.send_message(message.chat.id, "[перейти в каталог](https://zarina.ru/man/clothes/)", reply_markup=menu_keyboard)  # Можно менять текст
+
+
+@bot.message_handler(func=lambda message: text_button_2 == message.text)
+def help_command(message):
+    bot.send_message(message.chat.id, "[перейти в каталог](https://zarina.ru/)", reply_markup=menu_keyboard)  # Можно менять текст
+
+
+@bot.message_handler(func=lambda message: text_button_3 == message.text)
+def help_command(message):
+    bot.send_message(message.chat.id, "[перейти в каталог](https://www.sela.ru/eshop/kids/)", reply_markup=menu_keyboard)  # Можно менять текст
+
+
+bot.add_custom_filter(custom_filters.StateFilter(bot))
+bot.add_custom_filter(custom_filters.TextMatchFilter())
+
+bot.infinity_polling()
+
